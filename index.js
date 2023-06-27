6ca7d06c48d892a43fbe3de9925aa8b5947b243f
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2023-06-27 14:29:48 +0530

    rechecking deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.get('/',(req,res)=>{
-  res.send({Trying : "to render the edit in deployment"})
+  res.send({Deployment : "rechecking deployment"})
 })
 
 app.listen(PORT);
